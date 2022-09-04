b971a921bf57396b2569558fa41d8ec4b00dc820
Thirumalai-Shaktivel <thirumalaiShaktivel@gmail.com>
2022-09-04 19:18:37 +0530

    Add missing </div> in index.php

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ include "Teacher Login/register.php";
     <div class="jumbotron">
       <div class="row">
         <div class="col-3">
-          <div class="nav flex-column nav-pills" aria-orientation="vertical">
+          <div class="nav flex-column nav-pills">
             <a class="nav-link active" href="#AI_ML" data-toggle="pill">Computer Science and Engineering</a>
             <a class="nav-link" href="#CSD" data-toggle="pill">Computer Science and Design</a>
             <a class="nav-link" href="#CSE" data-toggle="pill">Artificial Intelligence & Machine Learning Engineering</a>
@@ -87,30 +87,29 @@ include "Teacher Login/register.php";
           <div class="tab-content" id="v-pills-tabContent">
             <div class="tab-pane fade show active" id="AI_ML">
               <div class="card">
-                <!-- <img class="card-img-top" src="image/AIML.jpg" alt="AI & ML image"> -->
                 <h5 class="card-header">Computer Science and Engineering</h5>
                 <div class="card-body">
                   <div class="row text-center">
-                      <div class="col-md-6">
-                        <h2 class="display-6">Teacher</h2>
-                        <div class="text-center col-12">
-                          <img src="image/Teacher.jpg" class="responsive" alt="Students">
+                    <div class="col-md-6">
+                      <h2 class="display-6">Teacher</h2>
+                      <div class="text-center col-12">
+                        <img src="image/Teacher.jpg" class="responsive" alt="Students">
+                      </div>
+                      <div class="form-row">
+                        <div class="form-group col">
+                            <button type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#TeacherLogin">
+                                Login
+                              </button>
                         </div>
-                        <div class="form-row">
-                          <div class="form-group col">
-                              <button type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#TeacherLogin">
-                                  Login
-                                </button>
-                          </div>
                         <div class="modal fade" id="TeacherLogin" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="TeacherLoginLabel" aria-hidden="true">
                           <div class="modal-dialog modal-dialog-centered modal-lg">
                             <div class="modal-content">
                               <form action="" method="post">
                                   <div class="modal-header">
-                                  <h5 class="modal-title" id="TeacherLoginLabel">Teacher Login</h5>
-                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                      <span aria-hidden="true">&times;</span>
-                                  </button>
+                                    <h5 class="modal-title" id="TeacherLoginLabel">Teacher Login</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
                                   </div>
                                   <div class="modal-body text-left">
                                       <div class="form-group">
@@ -130,121 +129,123 @@ include "Teacher Login/register.php";
                             </div>
                           </div>
                         </div>
-                          <div class="form-group col ">
-                              <button type="button" class="btn btn-outline-success btn-block" data-toggle="modal" data-target="#TeacherSignUp">
-                                  Sign Up
-                                </button>
-                          </div>
-                          <div class="modal fade" id="TeacherSignUp" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="TeacherSignUpLabel" aria-hidden="true">
-                              <div class="modal-dialog modal-dialog-centered modal-lg">
-                                <div class="modal-content">
-                                  <form action="" method="post">
-                                      <div class="modal-header">
-                                      <h5 class="modal-title" id="TeacherSignUpLabel">Teacher SignUp</h5>
-                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                          <span aria-hidden="true">&times;</span>
-                                      </button>
-                                      </div>
-                                      <div class="modal-body text-left">
-                                          <div class="mb-3">
-                                              <h4 class="form-label">User Name</h4>
-                                              <input type="text" name="username" class="form-control" placeholder="Enter User Name" required>
-                                            </div>
-                                            <div class="mb-3">
-                                              <h4 class="form-label">New Password</h4>
-                                              <input type="password" name="password" class="form-control" placeholder="Enter New Password" required>
-                                            </div>
-                                            <div class="mb-3">
-                                              <h4 class="form-label">Confirm Password</h4>
-                                              <input type="password" name="confirm_password" class="form-control" placeholder="Confirm New Password" required>
-                                            </div>
-                                      </div>
-                                      <div class="modal-footer">
-                                          <button type="submit" name="TeacherSignUp" class="btn btn-primary">Sign Up</button>
-                                          <button data-dismiss="modal" type="button" class="btn btn-outline-danger">Cancel</a>
-                                      </div>
-                                  </form>
-                                </div>
-                              </div>
-                            </div>
+                        <div class="form-group col ">
+                            <button type="button" class="btn btn-outline-success btn-block" data-toggle="modal" data-target="#TeacherSignUp">
+                                Sign Up
+                              </button>
                         </div>
-                      </div>
-                      <div class="col-md-6">
-                        <h2 class="display-6">Student</h2>
-                        <div class="text-center col-12">
-                            <img src="image/College_Students.jpg"  width="490"  class="responsive" alt="Students">
-                        </div>
-                        <div class="form-row">
-                          <div class="form-group col">
-                              <button type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#StudentLogin">
-                                  Login
-                                </button>
-                          </div>
-                          <div class="modal fade" id="StudentLogin" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="StudentLoginLabel" aria-hidden="true">
-                              <div class="modal-dialog modal-dialog-centered modal-lg">
-                                <div class="modal-content">
-                                  <form action="" method="post">
-                                      <div class="modal-header">
-                                      <h5 class="modal-title" id="StudentLoginLabel">Student Login</h5>
-                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                          <span aria-hidden="true">&times;</span>
-                                      </button>
-                                      </div>
-                                      <div class="modal-body text-left">
-                                          <div class="form-group">
-                                            <h4>Student ID</h4>
-                                            <input type="text" name="uniqueId" class="form-control" placeholder="Enter Student ID *" required>
-                                          </div>
-                                          <div class="form-group ">
-                                            <h4>Password</h4>
-                                            <input type="password" name="password" class="form-control" placeholder="Enter Password *" required>
-                                          </div>
-                                      </div>
-                                      <div class="modal-footer">
-                                          <button type="submit" name="StudentLogin" class="btn btn-primary">Log In</button>
-                                          <button data-dismiss="modal" type="button" class="btn btn-outline-danger">Cancel</a>
-                                      </div>
-                                  </form>
-                                </div>
-                              </div>
-                            </div>
-                      <div class="form-group col">
-                          <button type="button" class="btn btn-outline-success btn-block" data-toggle="modal" data-target="#StudentSignUp">
-                              Sign Up
-                            </button>
-                      </div>
-                      <div class="modal fade" id="StudentSignUp" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="StudentSignUpLabel" aria-hidden="true">
+                        <div class="modal fade" id="TeacherSignUp" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="TeacherSignUpLabel" aria-hidden="true">
                           <div class="modal-dialog modal-dialog-centered modal-lg">
                             <div class="modal-content">
                               <form action="" method="post">
                                   <div class="modal-header">
-                                  <h5 class="modal-title" id="StudentSignUpLabel">Student SignUp</h5>
-                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                      <span aria-hidden="true">&times;</span>
-                                  </button>
+                                    <h5 class="modal-title" id="TeacherSignUpLabel">Teacher SignUp</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
                                   </div>
                                   <div class="modal-body text-left">
                                       <div class="mb-3">
-                                          <h4 class="form-label">Student ID</h4>
-                                          <input type="text" name="uniqueId" class="form-control" placeholder="Enter Student ID" required>
-                                        </div>
-                                        <div class="mb-3">
-                                          <h4 class="form-label">New Password</h4>
-                                          <input type="password" name="password" class="form-control" placeholder="Enter New Password" required>
-                                        </div>
-                                        <div class="mb-3">
-                                          <h4 class="form-label">Confirm Password</h4>
-                                          <input type="password" name="confirm_password" class="form-control" placeholder="Confirm New Password" required>
-                                        </div>
+                                          <h4 class="form-label">User Name</h4>
+                                          <input type="text" name="username" class="form-control" placeholder="Enter User Name" required>
+                                      </div>
+                                      <div class="mb-3">
+                                        <h4 class="form-label">New Password</h4>
+                                        <input type="password" name="password" class="form-control" placeholder="Enter New Password" required>
+                                      </div>
+                                      <div class="mb-3">
+                                        <h4 class="form-label">Confirm Password</h4>
+                                        <input type="password" name="confirm_password" class="form-control" placeholder="Confirm New Password" required>
+                                      </div>
                                   </div>
                                   <div class="modal-footer">
-                                      <button type="submit" name="StudentSignUp" class="btn btn-primary">Sign Up</button>
+                                      <button type="submit" name="TeacherSignUp" class="btn btn-primary">Sign Up</button>
                                       <button data-dismiss="modal" type="button" class="btn btn-outline-danger">Cancel</a>
                                   </div>
                               </form>
                             </div>
                           </div>
                         </div>
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <h2 class="display-6">Student</h2>
+                      <div class="text-center col-12">
+                          <img src="image/College_Students.jpg"  width="490"  class="responsive" alt="Students">
+                      </div>
+                      <div class="form-row">
+                        <div class="form-group col">
+                            <button type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#StudentLogin">
+                                Login
+                              </button>
+                        </div>
+                        <div class="modal fade" id="StudentLogin" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="StudentLoginLabel" aria-hidden="true">
+                          <div class="modal-dialog modal-dialog-centered modal-lg">
+                            <div class="modal-content">
+                              <form action="" method="post">
+                                <div class="modal-header">
+                                  <h5 class="modal-title" id="StudentLoginLabel">Student Login</h5>
+                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                      <span aria-hidden="true">&times;</span>
+                                  </button>
+                                </div>
+                                <div class="modal-body text-left">
+                                    <div class="form-group">
+                                      <h4>Student ID</h4>
+                                      <input type="text" name="uniqueId" class="form-control" placeholder="Enter Student ID *" required>
+                                    </div>
+                                    <div class="form-group ">
+                                      <h4>Password</h4>
+                                      <input type="password" name="password" class="form-control" placeholder="Enter Password *" required>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="submit" name="StudentLogin" class="btn btn-primary">Log In</button>
+                                    <button data-dismiss="modal" type="button" class="btn btn-outline-danger">Cancel</a>
+                                </div>
+                              </form>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="form-group col">
+                            <button type="button" class="btn btn-outline-success btn-block" data-toggle="modal" data-target="#StudentSignUp">
+                                Sign Up
+                              </button>
+                        </div>
+                        <div class="modal fade" id="StudentSignUp" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="StudentSignUpLabel" aria-hidden="true">
+                          <div class="modal-dialog modal-dialog-centered modal-lg">
+                            <div class="modal-content">
+                              <form action="" method="post">
+                                <div class="modal-header">
+                                  <h5 class="modal-title" id="StudentSignUpLabel">Student SignUp</h5>
+                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                      <span aria-hidden="true">&times;</span>
+                                  </button>
+                                </div>
+                                <div class="modal-body text-left">
+                                  <div class="mb-3">
+                                      <h4 class="form-label">Student ID</h4>
+                                      <input type="text" name="uniqueId" class="form-control" placeholder="Enter Student ID" required>
+                                  </div>
+                                  <div class="mb-3">
+                                    <h4 class="form-label">New Password</h4>
+                                    <input type="password" name="password" class="form-control" placeholder="Enter New Password" required>
+                                  </div>
+                                  <div class="mb-3">
+                                    <h4 class="form-label">Confirm Password</h4>
+                                    <input type="password" name="confirm_password" class="form-control" placeholder="Confirm New Password" required>
+                                  </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="submit" name="StudentSignUp" class="btn btn-primary">Sign Up</button>
+                                    <button data-dismiss="modal" type="button" class="btn btn-outline-danger">Cancel</a>
+                                </div>
+                              </form>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
